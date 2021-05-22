317aedc8b74763233016dc20373b92013f6cc952
kk488 <78923924+kk488@users.noreply.github.com>
2021-05-21 18:14:28 -0700

    Update task4.py

--- task4.py
+++ task4.py
@@ -6,4 +6,12 @@ Return True if the number is an integer
 Return False if the number is not an integer
 (2 points)
 """
-
+def isInteger(number):
+    #input:float
+    #return value:true if the number is an integer;false otherwise
+    if number%1==0:
+        flag=True
+        return flag
+    else:
+        flag=False
+        return flag
