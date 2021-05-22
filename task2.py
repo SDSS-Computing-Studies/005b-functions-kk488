db2224d2ccc0b0f7e8e493fcc9149cee14fa53b6
kk488 <78923924+kk488@users.noreply.github.com>
2021-05-21 18:14:05 -0700

    Update task2.py

--- task2.py
+++ task2.py
@@ -6,3 +6,9 @@ The input is a list.
 The return value is the largest value in the list
 (2 points)
 """
+def largest(mylist):
+    #input
+    #mylist:list
+    #return value:returns the largest value in the list
+    largest=max(mylist)
+    return largest
